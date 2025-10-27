cfc655fa27e3e071530a571bb0fc6f50af65cfe5
rizeroAcc <kirya123bu@bk.ru>
2025-10-27 16:14:53 +0300

    Added hilt library tasklist module

--- tasklist/build.gradle.kts
+++ tasklist/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.android.hilt)
+    alias(libs.plugins.kotlin.ksp)
 }
 
 android {
@@ -37,6 +39,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.android.hilt)
+    ksp(libs.android.hilt.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
