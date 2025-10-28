e4c0a48df63872e9fccc41c573c1e03049f17c87
rizeroAcc <kirya123bu@bk.ru>
2025-10-28 12:09:12 +0300

    -Created test interfaces -Configured bottom navigation -Setup main navigation

---
+++ current-task-list/src/main/java/com/example/tasklist/presentation/components/TaskCard.kt
@@ -0,0 +1,55 @@
+package com.example.tasklist.presentation.components
+
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TaskCard(taskName : String, shortDescription : String,){
+    Card(modifier = Modifier
+        .drawBehind({Color.Red})
+        .fillMaxWidth()
+        .heightIn(50.dp,100.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.LightGray)
+        ) {
+        Column() {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 4.dp),
+                horizontalArrangement = Arrangement.Center) {
+                Text(taskName)
+            }
+            Text(
+                modifier = Modifier.
+                    padding(start = 8.dp).
+                    scrollable(rememberScrollableState { 0.0f }, orientation = Orientation.Horizontal),
+                text = shortDescription
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TaskCardPreview(){
+    TaskCard(taskName = "Имя задания", shortDescription = "Сделай че-то")
+}
--- task-feature/build.gradle.kts
+++ task-feature/build.gradle.kts
@@ -1,13 +1,10 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.android.hilt)
-    alias(libs.plugins.kotlin.ksp)
 }
 
 android {
-    namespace = "com.example.tasklist"
+    namespace = "com.example.task_feature"
     compileSdk = 36
 
     defaultConfig {
@@ -36,29 +33,10 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-
-    implementation(libs.android.hilt)
-
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.activity.compose)
-
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-
-    implementation(libs.androidx.navigation3.runtime)
-
-    ksp(libs.android.hilt.compiler)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(project(":core-navigation"))
 }
